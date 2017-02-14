e5c23942d277e05f5fab9bbf0a4212c51b9b9816
Ben <bjohnson@r105u03.dsra.local>
2017-02-14 15:52:14 -0500

    ...

--- pyDarknet/run-test.sh
+++ pyDarknet/run-test.sh
@@ -4,9 +4,8 @@ MPATH="/home/bjohnson/projects/darknet-bkj/custom-tools/pfr-data/backup"
 
 IMG_DIR=f
 IMG_PATH="/home/bjohnson/projects/sm-analytics/etl/kafka/data/imgs/s3.amazonaws.com/pipeline-image-store/images/default/default/$IMG_DIR"
-find $IMG_PATH -type f > test.txt
 
 for weights in $(ls $MPATH | shuf); do
     f=$(echo $weights | cut -d'.' -f1)
-    cat test.txt | ./detect-thread.py --weight-path $MPATH/$weights  > ./results/results-$f
+    find $IMG_PATH -type f | ./detect-thread.py --weight-path $MPATH/$weights  > ./results/results-$f
 done
