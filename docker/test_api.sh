4a15734e41366b1ffafc1be28823f4d3512e0765
Ben Johnson <bkj.322@gmail.com>
2017-07-14 14:10:08 -0400

    Update test_api.sh

--- docker/test_api.sh
+++ docker/test_api.sh
@@ -2,6 +2,6 @@
 
 curl -XPOST -H "Content-Type:application/json" http://localhost:5000/api/score -d '{
     "urls" : [
-        "http://i.fakesite.co.uk/multimedia/archive/03166/isil_3166282b.jpg"
+        "http://fakesite.com/test.jpg"
     ]
 }' | jq .
